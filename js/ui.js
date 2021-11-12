62cc9801f8508d84f935c2d5a20081bb77a70d6f
hemmiklemmi <hemmioskar@gmail.com>
2021-11-12 15:18:06 +0000

    hélt áfram með ui-ið og virkni

--- js/ui.js
+++ js/ui.js
@@ -6,6 +6,8 @@ export async function fetchAndCreatePage() {
   const data = await getData();
   importData(data);
   createCategories(data);
+  showProjects();
+  createNewProjectBtn();
   return data;
 }
 /**
@@ -16,9 +18,9 @@ function createCategories(data) {
   const counts = countData(data);
   for (let i = 0; i < data.categories.length; i+=1) {
     const ge = document.createElement('div');
-    ge.classList.add('category__container');
+    ge.classList.add('category-container');
     const newCat = el('h3', data.categories[i].title);
-    const he = document.querySelector('.project__categories');
+    const he = document.querySelector('.project-categories');
     he.append(ge);
     ge.append(newCat);
     for (const items in counts.catCount) {
@@ -31,19 +33,19 @@ function createCategories(data) {
   for (const items in counts.tagCount) {
       if(items){
         const ge = document.createElement('div');
-        ge.classList.add('category__container');
+        ge.classList.add('category-container');
         const newCat = el('h3', items);
-        const he = document.querySelector('.project__tags');
+        const he = document.querySelector('.project-tags');
         he.append(ge);
         ge.append(newCat);
         const counter = el('p', `${counts.tagCount[items]}`);
         ge.append(counter);
       }
   }
-  const projectCount = document.querySelector('.project__count');
+  const projectCount = document.querySelector('.project-count');
   projectCount.textContent = counts.count;
 
-  const unfinishedCount = document.querySelector('.unfinished__count');
+  const unfinishedCount = document.querySelector('.unfinished-count');
   unfinishedCount.textContent = counts.finishedCount;
 }
 
@@ -77,4 +79,52 @@ function countData(data) {
     }
   }
   return { tagCount, catCount, count, finishedCount };
+}
+function showProjects(id = ''){
+    const ul = document.createElement('ul');
+    ul.classList.add('projects');
+    const container= document.querySelector('.projects-container')
+    container.append(ul);
+    for(let i = 1; i<window.localStorage.length; i+= 1){
+        const parsedItem = JSON.parse(window.localStorage.getItem(i));
+        const newLi = el('li','');
+        const title = el('h3',  parsedItem.title)
+        ul.appendChild(newLi);
+        newLi.append(title);
+        const dateTagsContainer = el('div', '');
+        dateTagsContainer.classList.add('date-tag-container');
+
+        if(parsedItem.description !== ''){
+            const descript = el('p' , parsedItem.description);
+            newLi.appendChild(descript);
+        }
+        newLi.append(dateTagsContainer);
+        const date = new Date(parsedItem.due)
+        const dateString =(date.toString()).split(' ');
+        const dateTime = el('p' , dateString[2], ' ', dateString[1]);
+        dateTagsContainer.append(dateTime);
+        
+        for(const item in parsedItem.tags){
+            if(parsedItem.tags !== ''){
+                const tag = el('button', parsedItem.tags[item])
+                dateTagsContainer.append(tag);
+            }
+        }
+        if(parsedItem.category !== ''){
+          const category = el('p' , parsedItem.category);
+          dateTagsContainer.appendChild(category);
+        }
+    }
+    
+}
+function createNewProjectBtn(){
+  const newProjectBtn = el('button', 'Búa til nýtt verkefni');
+  const ul = document.querySelector('.projects');
+  ul.append(newProjectBtn);
+  newProjectBtn.classList.add('new-modify-project');
+  newProjectBtn.addEventListener('click', ()=>{
+    const he = document.querySelector('.new-project');
+    he.classList.remove('hidden');
+    ul.classList.add('hidden');
+  } )
 }
--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 import { fetchAndCreatePage } from './js/ui.js';
 
 fetchAndCreatePage();
+const addBtn = document.querySelector('.add-btn');
+addBtn.addEventListener('click', () => {
+    const projects = document.querySelector('.projects');
+    const newProjects =document.querySelector('.new-project');
+    projects.classList.remove('hidden')
+    newProjects.classList.add('hidden');
+})

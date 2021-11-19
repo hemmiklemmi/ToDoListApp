c59c0e9c8d10a5c9b0be337c05e49241d6161f87
unknown <binnibja@gmail.com>
2021-11-19 23:20:07 +0000

    i

--- js/ui.js
+++ js/ui.js
@@ -143,8 +143,8 @@ export function modifyProject(id) {
   const modify = document.querySelector('.modify-project');
   const he = document.querySelector('.new-project');
   const ul = document.querySelector('.projects');
-  let title = document.getElementById('titill').value
   let item = JSON.parse(localStorage.getItem(id));
+  console.log(item);
   modify.classList.remove('hidden');
   he.classList.add('hidden');
   ul.classList.add('hidden');
